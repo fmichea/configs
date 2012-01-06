bdaa5576440c9b4a4640532743cdae26a0d84dba
Franck Michea <franck.michea@gmail.com>
2012-01-06 22:20:06 +0100

    Update: making qemu window floating.

--- awesome/main.lua
+++ awesome/main.lua
@@ -188,6 +188,10 @@ awful.rules.rules = {
         rule = { class = "gimp" },
         properties = { floating = true }
     },
+    {
+        rule = { class = "qemu" },
+        properties = { floating = true }
+    },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
