e9becb7ac8d056c824a301d415a2e8e4c750f91c
Franck Michea <franck.michea@gmail.com>
2012-02-17 15:42:17 +0100

    Add: spizaetos-ornatus' widgets for awesome.

---
+++ awesome/widgets.d/spizaetus-ornatus.lua
@@ -0,0 +1,11 @@
+-- {{{ Spizaetos-ornatus' Widget Configuration
+
+widgets = {
+    separator, netuplabel, netwidget, netdownlabel, separator,
+    memwidget, memlabel, separator,
+    cpuwidget, cpulabel, separator,
+    layout = awful.widget.layout.horizontal.rightleft
+}
+
+return widgets
+-- }}}
