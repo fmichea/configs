041ea6ef18c3093db07337756643cce8b5a2d888
Franck Michea <franck.michea@gmail.com>
2015-06-07 00:49:56 +0200

    Add platforms capability.

--- confmgr.py
+++ confmgr.py
@@ -15,8 +15,30 @@ _FILES_DIR = os.path.join(_ROOT, 'files')
 _HOME = os.path.expanduser('~')
 
 _SECTION = re.compile('^(?P<section_type>\w+):(?P<name>[a-zA-Z0-9\.-]+)$')
+_LIST_OF_VALUES = re.compile('\s*,\s*')
 
 _PLATFORMS = ['linux', 'darwin']
+
+
+class Status(object):
+    INSTALLED = 'installed'
+    NOT_AVAILABLE_FOR_PLATFORM = 'not available for platform'
+    NOT_INSTALLED = 'not installed'
+    SOURCE_NOT_FOUND = 'source not found'
+    UNKNOWN_NOT_A_SYMLINK = 'unkown: not a symlink'
+    UNKNOWN_NOT_LINKED_TO_SOURCE = 'unknown: not linked to source'
+
+
+def _parse_platforms(value):
+    if value is None:
+        return _PLATFORMS
+    values = _LIST_OF_VALUES.split(value)
+    values = set([v for v in values if v.strip() != ''])
+    for value in values:
+        if value not in _PLATFORMS:
+            _debug('Value {} is not a valid platform.'.format(value))
+            return _PLATFORMS
+    return values
 
 
 def _debug(*a, **kw):
@@ -40,9 +62,18 @@ class ConfigurationFile:
         self._soft_conf, self.name, self._conf = soft_conf, name, conf
         # generate filename
         self.source_filename = self._conf.get('filename', self.name)
+        self.platforms = self._conf.get('platforms')
         self.target_filename = self.source_filename
         if not self._conf.getboolean('no_dot', False):
             self.target_filename = '.{}'.format(self.target_filename)
+
+    @property
+    def platforms(self):
+        return self._platforms
+
+    @platforms.setter
+    def platforms(self, value):
+        self._platforms = _parse_platforms(value)
 
     @property
     def target_path(self):
@@ -52,18 +83,36 @@ class ConfigurationFile:
     def source_path(self):
         return os.path.join(_FILES_DIR, self.source_filename)
 
+    def can(self, action, force=False):
+        _not_installable = [
+            Status.INSTALLED,
+            Status.NOT_AVAILABLE_FOR_PLATFORM,
+            Status.SOURCE_NOT_FOUND,
+        ]
+        if action == 'install':
+            if not force and self.status == Status.NOT_INSTALLED:
+                return True
+            if force and self.status not in _not_installable:
+                return True
+            return False
+        if action == 'uninstall':
+            return self.status == 'installed'
+        return False
+
     @property
     def status(self):
         source_path, target_path = self.source_path, self.target_path
+        if sys.platform not in self.platforms:
+            return Status.NOT_AVAILABLE_FOR_PLATFORM
         if not os.path.exists(source_path):
-            return 'source not found'
+            return Status.SOURCE_NOT_FOUND
         if not os.path.lexists(target_path):
-            return 'not installed'
+            return Status.NOT_INSTALLED
         if not os.path.islink(target_path):
-            return 'unkown: not a symlink'
+            return Status.UNKNOWN_NOT_A_SYMLINK
         if os.path.realpath(target_path) != source_path:
-            return 'unknown: not linked to source'
-        return 'installed'
+            return Status.UNKNOWN_NOT_LINKED_TO_SOURCE
+        return Status.INSTALLED
 
 
 class SoftwareConfiguration:
@@ -86,6 +135,7 @@ class SoftwareConfiguration:
                 self.files[conf_file.name] = conf_file
             if section_type == 'software':
                 self.name = name
+                self.platforms = section.get('platforms')
                 self.base_path = section.get('base_path', _HOME)
                 self.setup_commands = section.get('setup-commands', '')
                 self.setup_commands = self.setup_commands.strip().splitlines()
@@ -95,8 +145,18 @@ class SoftwareConfiguration:
         # all the defaults.
         if self.name is None:
             self.name = os.path.basename(filename)[:-len('.ini')]
+            self.platforms = None
             self.base_path = _HOME
             self.setup_commands = []
+            self.teardown_commands = []
+
+    @property
+    def platforms(self):
+        return self._platforms
+
+    @platforms.setter
+    def platforms(self, value):
+        self._platforms = _parse_platforms(value)
 
 
 def load_software_configurations():
@@ -116,7 +176,12 @@ def _iter_softwares(args, softwares):
         try:
             soft = softwares[soft_name]
         except KeyError:
-            _print('[warn] Software `%s` does not exist.', soft_name, file=sys.stderr)
+            warn = '[warn] Software `%s` does not exist.'
+            _print(warn.format(soft_name), file=sys.stderr)
+            continue
+        if sys.platform not in soft.platforms:
+            warn = '[warn] Software `{}` is not available for this platform.'
+            _print(warn.format(soft_name), file=sys.stderr)
             continue
         yield soft_name, soft
 
@@ -125,8 +190,10 @@ def info_command(args, softwares):
     for soft_name, soft in _iter_softwares(args, softwares):
         _print('Software:', soft_name)
         _print('Base:', soft.base_path, tabs=1)
+        _print('Platforms:', ', '.join(soft.platforms), tabs=1)
         for filename, conf_file in soft.files.items():
             _print('Configuration filename:', filename, tabs=1)
+            _print('platforms:', ', '.join(conf_file.platforms), tabs=2)
             _print('source:', conf_file.source_path, tabs=2)
             _print('target:', conf_file.target_path, tabs=2)
             _print('status:', conf_file.status, tabs=2)
@@ -140,16 +207,20 @@ def install_command(args, softwares):
             _print('=>', 'Executing:', command, tabs=1)
             os.system(command)
         for filename, conf_file in soft.files.items():
-            should_install = (not args.force and conf_file.status == 'not installed')
-            should_install = should_install or (args.force and conf_file.status not in ['installed', 'source not found'])
             try:
-                if should_install:
-                    _print('=>', 'Installing', conf_file.source_path, 'as', conf_file.target_path, tabs=1)
+                if conf_file.can('install', force=args.force):
+                    _print('=> Installing {0} as {1}'.format(
+                        conf_file.source_path,
+                        conf_file.target_path,
+                    ), tabs=1)
                     if os.path.lexists(conf_file.target_path):
                         os.unlink(conf_file.target_path)
                     os.symlink(conf_file.source_path, conf_file.target_path)
                 else:
-                    _print('=>', 'Skipping', conf_file.source_path, 'because of status:', conf_file.status, tabs=1)
+                    _print('=> Skipping {0} because of status: {1}'.format(
+                        conf_file.source_path,
+                        conf_file.status,
+                    ), tabs=1)
             except (IOError, OSError) as exc:
                 msg = '/!\ Could not install {0} [{1}]: {2}'.format(
                     conf_file.target_path, type(exc).__name__, exc,
@@ -162,11 +233,14 @@ def uninstall_command(args, softwares):
         _print('Uninstalling software', soft_name, 'from', soft.base_path)
         for filename, conf_file in soft.files.items():
             try:
-                if conf_file.status == 'installed':
-                    _print('=>', 'Uninstalling', conf_file.target_path, tabs=1)
+                if conf_file.can('uninstall'):
+                    _print('=> Uninstalling', conf_file.target_path, tabs=1)
                     os.unlink(conf_file.target_path)
                 else:
-                    _print('=>', 'Skipping', conf_file.source_path, 'because of status:', conf_file.status, tabs=1)
+                    _print('=> Skipping {0} because of status: {1}'.format(
+                        conf_file.source_path,
+                        conf_file.status
+                    ), tabs=1)
             except (IOError, OSError) as exc:
                 msg = '/!\ Could not uninstall {0} [{1}]: {2}'.format(
                     conf_file.target_path, type(exc).__name__, exc,
